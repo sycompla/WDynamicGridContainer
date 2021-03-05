aea21ece3ecf88f2be82ac3fcecd852b18bb9548
d7p4n4 <Szappan60>
2021-03-05 16:52:47 +0100

    Initial commit

---
+++ sources/controllers/view1.controller.js
@@ -0,0 +1,34 @@
+sap.ui.controller("sources.controllers.view1", {
+    onInit: function(oEvent) {
+
+        this._oPnl = this.byId("ScrollP");
+    },
+    addInput: function(){
+        console.log("clicked")
+        oInput1 = new sap.m.Input();/*
+        delIcon = new sap.ui.core.Icon({
+            src:"sap-icon://delete",
+            press:this.onDeleteCcMail
+        });*/
+        _oCcLayout = new sap.m.FlexBox({
+            alignItems:"Center",
+            justifyContent:"Start",
+            items:[oInput1]
+        });
+        this._oPnl.addContent(_oCcLayout);
+    },
+    getValue: function(){
+        debugger;
+        var values = "";
+        var pnlDom = this._oPnl.getDomRef()
+        $(pnlDom).find('input').each(function(index, elem) {
+            debugger;
+            values += ", " + $(elem)[0].value;
+        });
+        alert(values);
+    },
+    onDeleteCcMail: function(oEvent){
+        var rowItemContainer = oEvent.getSource().getParent();
+        rowItemContainer.destroy();
+    }
+});
