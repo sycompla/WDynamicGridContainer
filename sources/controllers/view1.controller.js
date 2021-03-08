8ff68bb4b3835edb3aa3c3daa4ac3ef3da30ca31
d7p4n4 <Szappan60>
2021-03-08 13:16:29 +0100

    1.20210308.1

--- sources/controllers/view1.controller.js
+++ sources/controllers/view1.controller.js
@@ -1,34 +1,49 @@
-sap.ui.controller("sources.controllers.view1", {
-    onInit: function(oEvent) {
+sap.ui.define(['sap/ui/core/mvc/Controller'],
+    function(Controller) {
+        "use strict";
+        var PageController = Controller.extend("sources.controllers.view1", {
+            onInit: function (oEvent) {
 
-        this._oPnl = this.byId("grid1");
-    },
-    addInput: function(){
-        console.log("clicked")
-        oInput1 = new sap.m.Input();/*
+                this._oPnl = this.byId("grid1");
+            },
+            addInput: function () {
+                console.log("clicked")
+                let oInput1 = new sap.m.Input();/*
         delIcon = new sap.ui.core.Icon({
             src:"sap-icon://delete",
             press:this.onDeleteCcMail
         });*/
-        _oCcLayout = new sap.m.FlexBox({
-            alignItems:"Center",
-            justifyContent:"Start",
-            items:[oInput1]
+                let genericTile = new sap.m.GenericTile({
+                    header: 'Goal Completion',
+                    subheader: 'Percentage completion of goals',
+                    headerImage: 'sap-icon://goal',
+                    press: function () {
+                        alert();
+                    }
+                });
+
+
+                    let _oCcLayout = new sap.m.FlexBox({
+                        alignItems: "Center",
+                        justifyContent: "Start",
+                        items: [genericTile]
+                    });
+                this._oPnl.addItem(genericTile);
+            },
+            getValue: function () {
+                debugger;
+                var values = "";
+                var pnlDom = this._oPnl.getDomRef()
+                $(pnlDom).find('input').each(function (index, elem) {
+                    debugger;
+                    values += ", " + $(elem)[0].value;
+                });
+                alert(values);
+            },
+            onDeleteCcMail: function (oEvent) {
+                var rowItemContainer = oEvent.getSource().getParent();
+                rowItemContainer.destroy();
+            }
         });
-        this._oPnl.addItem(_oCcLayout);
-    },
-    getValue: function(){
-        debugger;
-        var values = "";
-        var pnlDom = this._oPnl.getDomRef()
-        $(pnlDom).find('input').each(function(index, elem) {
-            debugger;
-            values += ", " + $(elem)[0].value;
-        });
-        alert(values);
-    },
-    onDeleteCcMail: function(oEvent){
-        var rowItemContainer = oEvent.getSource().getParent();
-        rowItemContainer.destroy();
-    }
+        return PageController;
 });
