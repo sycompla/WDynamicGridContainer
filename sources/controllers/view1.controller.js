de849d7ec64ed701263778c5915e14fe77ca91af
d7p4n4 <Szappan60>
2021-03-08 15:49:07 +0100

    1.20210308.4

--- sources/controllers/view1.controller.js
+++ sources/controllers/view1.controller.js
@@ -8,46 +8,24 @@ sap.ui.define(['sap/ui/core/mvc/Controller'],
             },
             addInput: function () {
                 console.log("clicked")
-                let oInput1 = new sap.m.Input();/*
-        delIcon = new sap.ui.core.Icon({
-            src:"sap-icon://delete",
-            press:this.onDeleteCcMail
-        });*/
+                this.addTile("bármi")
+            },
+            addTile: function (title) {
+                let oInput1 = new sap.m.Input();
                 let genericTile = new sap.m.GenericTile({
-                    header: 'A very looooooooooooooooooooooooooooooooooooooooooong header',
-                    subheader: 'Percentage completion of goals',
+                    header: title,
+                    subheader: '',
                     headerImage: 'sap-icon://goal',
                     press: function () {
                         alert();
                     },
                     layoutData: new sap.f.GridContainerItemLayoutData({
-                        minRows: 6,
-                        columns: 4
+                        minRows: 2,
+                        columns: 2
                     })
                 });
-
-
-                    let _oCcLayout = new sap.m.FlexBox({
-                        alignItems: "Center",
-                        justifyContent: "Start",
-                        items: [genericTile]
-                    });
                 this._oPnl.addItem(genericTile);
             },
-            getValue: function () {
-                debugger;
-                var values = "";
-                var pnlDom = this._oPnl.getDomRef()
-                $(pnlDom).find('input').each(function (index, elem) {
-                    debugger;
-                    values += ", " + $(elem)[0].value;
-                });
-                alert(values);
-            },
-            onDeleteCcMail: function (oEvent) {
-                var rowItemContainer = oEvent.getSource().getParent();
-                rowItemContainer.destroy();
-            }
-        });
+        })
         return PageController;
 });
